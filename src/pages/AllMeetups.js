2a5face2ff433050cc9baaa2f4bc5b39e9b5bb85
Kevin <kbeck1960@gmail.com>
2021-12-13 13:47:40 -0500

    complete app

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+
+import MeetupList from '../components/layout/meetups/MeetupList';
+
+export default AllMeetupsPage;
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://react---demo-default-rtdb.firebaseio.com/meetups.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
